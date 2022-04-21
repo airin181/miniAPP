11c07aa4680ef025c4e1082214cfdfa7925ff5f5
airin181 <98608110+airin181@users.noreply.github.com>
2022-04-21 18:08:34 +0200

    ADD FIRESTORAGE FEATURE

--- utils/firestore.js
+++ utils/firestore.js
@@ -1,23 +1,6 @@
-/* // Import the functions you need from the SDKs you need
-const {initializeApp} = require('firebase/app')
-require('firebase/storage')
+const { initializeApp } = require("firebase/app");
+const firebaseConfig = require('../config/config-firebase');
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const db = initializeApp(firebaseConfig);
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
-};
-
-
-firebase.initializeApp(firebaseConfig);
-
-const storage = firebase.storage();
-
-module.exports = storage, firebase; */
+module.exports = db;
